8379284f80429b744aeeddec20ca68530783ab76
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-01 18:12:00 -0300

    Adding exercise partI - 1

---
+++ Introduction - Web Development/Block-04/Day-04/exercises-part1.js
@@ -0,0 +1,16 @@
+// @Autor: Rafael Machado Guimarães - rafaelmguimaraes@gmail.com
+// Exercises Part I - Objects and For / In
+
+//Using the object below, do the following exercises:
+ 
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  }; 
+
+//1. Print a welcome message on the console to the character above, including her name.
+// Expected value on the console: Bem-vinda, Margarida
+
+console.log(`Welcome, ${info.personagem}`);
+  
