61362a28ea64de709f6694e093bc3add46865007
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-01 16:16:18 -0300

    Change exercise to use function - subtracao

--- Introduction - Web Development/Block-04/Day-01/subtracao.js
+++ Introduction - Web Development/Block-04/Day-01/subtracao.js
@@ -1,4 +1,12 @@
+/* 
 let a = 10;
 let b = 20;
 
-console.log(a - b);
+console.log(a - b); 
+*/
+
+function subFuntion (a, b) {
+    return  a - b;   
+};
+
+console.log(`Test subFunction with a=20 e b=12, result is ${subFuntion(20,12)}.`);
