9307580fff7b3b2973af5b34b72da511dbe1c8b7
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-01 16:19:07 -0300

    Change exercise to use function - triangulo

--- Introduction - Web Development/Block-04/Day-01/triangulo.js
+++ Introduction - Web Development/Block-04/Day-01/triangulo.js
@@ -2,6 +2,7 @@
 //ângulos internos de um triângulo. Retorne true se os ângulos 
 //representarem os ângulos de um triângulo e false caso contrário.
 
+/* 
 let anguloA = 80;
 let anguloB = 55;
 let anguloC = 45;
@@ -9,4 +10,12 @@ let anguloC = 45;
 
 console.log(anguloA + anguloB + anguloC == 180);
 
+ */
 
+ function ehTriangulo(a,b,c) {
+    return (a + b + c === 180);
+ };
+
+ console.log(ehTriangulo(80,55,45));
+ console.log(ehTriangulo(90,45,45));
+ console.log(ehTriangulo(70,62,45));
