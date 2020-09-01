43cab770abdd42cb0e295ea50a603b90ec8514fe
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-01 16:05:59 -0300

    Change exercise to use function - mod e mul

--- Introduction - Web Development/Block-04/Day-01/modulo.js
+++ Introduction - Web Development/Block-04/Day-01/modulo.js
@@ -1,4 +1,13 @@
+/* 
 let a = 10;
 let b = 20;
 
 console.log(a % b);
+
+ */
+
+function modFuntion (a, b) {
+    return  a % b;   
+};
+
+console.log(`Test modFunction with a=50 e b=10, result is ${modFuntion(50,10)}.`);
