4e0a4782a3f72b9b8057470373ae49c0f141273f
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-01 15:46:30 -0300

    Change exercise to use function - maior que 2 numeros

--- Introduction - Web Development/Block-04/Day-01/maior-de-dois-numeros.js
+++ Introduction - Web Development/Block-04/Day-01/maior-de-dois-numeros.js
@@ -1,6 +1,6 @@
 //Faça um programa que retorne o maior de dois números. 
 //Defina no começo do programa duas variáveis com os valores que serão comparados.
-
+/* 
 let a = 10;
 let b = 20;
 
@@ -10,4 +10,19 @@ if (a > b) {
     console.log("B");
   } else {
     console.log("A=B");
-  }
+  }; 
+  */
+
+  function maiorNumero(a,b) {
+    if (a > b) {
+      console.log("A");
+    } else if (a < b) {
+      console.log("B");
+    } else {
+      console.log("A=B");
+    };
+  };
+
+  maiorNumero(1,2);
+  maiorNumero(3,1);
+  maiorNumero(5,5);
