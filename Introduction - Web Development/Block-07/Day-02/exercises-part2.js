a7496686ee4e2b9be38931b06f0bb633d404aeb4
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-09-28 19:34:09 -0300

    Solving exercises 2 - Part 2

--- Introduction - Web Development/Block-07/Day-02/exercises-part2.js
+++ Introduction - Web Development/Block-07/Day-02/exercises-part2.js
@@ -26,3 +26,13 @@ const lesson1 = {
 const addProperty = (lessonObject, lessonProperty, lessonValue) => lessonObject[lessonProperty] = lessonValue;
 addProperty(lesson2,'turno','manhã');
 console.table(lesson2);
+
+// 2.Crie uma função para listar as keys de um objeto. 
+// Essa função deve receber um objeto como parâmetro.
+const showKeys = lessonObject => Object.keys(lessonObject);
+console.table(showKeys(lesson1));
+
+// 3.Crie uma função para mostrar o tamanho de um objeto.
+
+// 4,Crie uma função para listar os valores de um objeto. 
+// Essa função deve receber um objeto como parâmetro.
