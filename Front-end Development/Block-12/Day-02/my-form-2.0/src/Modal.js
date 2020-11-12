84d7d5dc3b03c1baf5b7ff9d3dd1f53980268341
rafaelmguimaraes <rafaelmguimaraes@gmail.com>
2020-11-12 12:10:52 -0300

    Include Modal and Screenshots

---
+++ Front-end Development/Block-12/Day-02/my-form-2.0/src/Modal.js
@@ -0,0 +1,16 @@
+// REFERENCE: https://www.digitalocean.com/community/tutorials/react-modal-component
+import './Modal.css'
+const Modal = ({ handleClose, show, children }) => {
+  const showHideClassName = show ? "modal display-block" : "modal display-none";
+
+  return (
+    <div className={showHideClassName}>
+      <section className="modal-main">
+        {children}
+        <button onClick={handleClose}>close</button>
+      </section>
+    </div>
+  );
+};
+
+export default Modal;
