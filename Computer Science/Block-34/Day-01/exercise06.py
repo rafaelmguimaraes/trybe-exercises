1c07fc4389f25e17602206be88bf0fdf30454fcb
Rafael M Guimarães <rafaelmguimaraes@gmail.com>
2021-06-28 20:17:24 -0300

    exercise 06 - lint

--- Computer Science/Block-34/Day-01/exercise06.py
+++ Computer Science/Block-34/Day-01/exercise06.py
@@ -1,9 +1,9 @@
 """
 Crie uma função que receba os três lado de um triângulo e informe qual o tipo
-de triângulo formado ou "não é triangulo" , caso não seja possível formar um 
+de triângulo formado ou "não é triangulo" , caso não seja possível formar um
 triângulo
 
-Três lados formam um triângulo quando a soma de quaisquer dois lados 
+Três lados formam um triângulo quando a soma de quaisquer dois lados
 for maior que o terceiro;
 Triângulo Equilátero: três lados iguais;
 Triângulo Isósceles: quaisquer dois lados iguais;
